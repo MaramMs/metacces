45681fbe4cb0db335fd03ed47a2434874232941c
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-08-22 22:51:17 +0400

    create home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,18 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
+import MainLayout from '../../components/MainLayout'
 
 export default function Home() {
+  const [openLayout, setOpenLayout] = useState(false);
+  const handleOpenLayout = () =>{
+    console.log('maram');
+    setOpenLayout(true)
+  }
+  console.log(openLayout);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between  bg-[url('/images/bg.png')] bg-no-repeat bg-center bg-cover">
+    <main className="flex min-h-screen flex-col items-center justify-between  bg-[url('/images/bg.png')] bg-no-repeat bg-cover">
+      
       <div className="image-container-center">
       <img src="/images/oli.png" alt="" className="oli" />
     </div> 
@@ -15,9 +25,12 @@ export default function Home() {
     <div className="image-container">
       <img src="/images/blue globe.png" alt="" className="glow-image" />
     </div>  
-    <div className='home-img-container'>
-      <img src="/images/oli home buttomn.png" alt="" className="home-image"/>
+    <div className='home-img-container' onClick={handleOpenLayout}>
+      <img src="/images/oli home buttomn.png" alt="" className="home-image" />
       </div> 
+      {
+        openLayout && <MainLayout />
+      }
     </main>
   )
 }

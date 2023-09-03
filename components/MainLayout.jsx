92e25e720e1f73ee0ad2fe48262db102018bb3f1
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-09-03 15:56:36 +0300

    fix issue

--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -7,7 +7,7 @@ import { Button } from "antd";
 import AddContent from "./AddContent";
 
 const MainLayout = () => {
-  const [addPage,setAddPage] = useState(true)
+  const [addPage,setAddPage] = useState(false)
   const handleAddPage =() =>{
     setAddPage(!addPage)
   }
